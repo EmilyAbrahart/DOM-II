1ea5a4f74b035aee0c46250bf197020933c6c41c
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-14 14:41:20 +0100

    Added link event listener and bus game

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 // Your code goes here
-TweenMax.to('.bus-header', 5, {left:450});
+
+// Header image animation
+TweenMax.to('.bus-header', 5, { left: 450, ease: Power4.easeOut });
+
+// Nav Items
+const navItems = document.querySelectorAll('.nav-link');
+const navMouse = function(event) {
+	event.target.style.color = 'red';
+	event.preventDefault();
+};
+
+navItems.forEach(link => {
+	link.addEventListener('mouseover', navMouse);
+});
+navItems.forEach(link => {
+	link.addEventListener(
+		'mouseout',
+		event => (event.target.style.color = 'black')
+	);
+});
+
+// Catch the Bus game
+const bus = document.querySelector('.catch-bus');
+const busGame = function (event){
+  event.target.style.top = `${Math.random()*600}px`;
+  event.target.style.left = `${Math.random()*600}px`;
+}
+
+bus.addEventListener('mouseover', busGame);
