2e412ee625f5baa88711bc30f12f0d3747502ebc
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-14 16:03:24 +0100

    Add modal for bus game instructions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 // Your code goes here
+// Modal
+const modalView = document.querySelector('.modal-content');
+const closeButton = document.querySelector('.closeBtn');
+function closeModal(){
+  modalView.style.display = 'none';
+}
+closeButton.addEventListener('dblclick', closeModal);
 
 // Header image animation
 TweenMax.to('.bus-header', 5, { left: 450, ease: Power4.easeOut });
@@ -22,9 +29,9 @@ navItems.forEach(link => {
 
 // Catch the Bus game
 const bus = document.querySelector('.catch-bus');
-const busGame = function (event){
-  event.target.style.top = `${Math.random()*600}px`;
-  event.target.style.left = `${Math.random()*600}px`;
-}
+const busGame = function(event) {
+	event.target.style.top = `${Math.random() * 600}px`;
+	event.target.style.left = `${Math.random() * 600}px`;
+};
 
 bus.addEventListener('mouseover', busGame);
